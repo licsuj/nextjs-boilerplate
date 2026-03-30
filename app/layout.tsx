ef3daee4fd55d350a54e865ad16b8ac39890616c
licsuj <licsuj27@proton.me>
2026-03-31 01:00:48 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "ELI5AI.co",
@@ -14,10 +14,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-        <GoogleAnalytics gaId="G-M0LJMK6XMQ" />
-      </body>
+      <body>{children}</body>
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-M0LJMK6XMQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-M0LJMK6XMQ');
+        `}
+      </Script>
     </html>
   );
 }
